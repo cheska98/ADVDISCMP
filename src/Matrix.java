a70b1a96a24faaea5bad6e964de0c910da047055
isakyaki <pamela_ortiz@dlsu.edu.ph>
2018-08-11 16:20:30 +0800

    transforming given list of vectors into a matrix

--- src/Matrix.java
+++ src/Matrix.java
@@ -39,25 +39,62 @@ public class Matrix {
 
     }
 
-    public Matrix(List<Vector> list, int dimension) {
+    public static double[][] transform(List<Vector> list, int dimension) {
 
+        double[][] temp = new double[dimension][dimension];
+
+        for (int i = 0; i < dimension; i++)
+            for (int j = 0; j <dimension; j++)
+                temp[j][i]= list.get(i).getArray()[j];
+
+        return temp;
+
+    }
+
+    public Matrix(List<Vector> list, int dimension) {
+        this.dimension = dimension;
+        this.array = transform(list, dimension);
     }
 
 
     public static void main(String[] args) {
 
-        Matrix m = new Matrix(5);
+//        Matrix m = new Matrix(5);
+//
+//
+//        double[][] x = m.getArray();
+//        for(int i = 0; i < m.getDimension(); i++) {
+//            for(int j = 0; j < m.getDimension(); j++) {
+//                System.out.print(x[i][j] + " ");
+//            }
+//
+//            System.out.println();
+//        }
 
+        List<Vector> vecList = new ArrayList<>();
+        int dimension = 3;
 
-        double[][] x = m.getArray();
-        for(int i = 0; i < m.getDimension(); i++) {
-            for(int j = 0; j < m.getDimension(); j++) {
-                System.out.print(x[i][j] + " ");
+        double[] arr1 = {1, 2, 4};
+        double[] arr2 = {1, 2, 6};
+        double[] arr3 = {0, 2, 3};
+
+        Vector vector1 = new Vector(dimension, arr1);
+        Vector vector2 = new Vector(dimension, arr2);
+        Vector vector3 = new Vector(dimension, arr3);
+
+        vecList.add(vector1);
+        vecList.add(vector2);
+        vecList.add(vector3);
+
+        Matrix n = new Matrix(vecList, dimension);
+        double[][] y = n.getArray();
+        
+        for(int i = 0; i < n.getDimension(); i++) {
+            for(int j = 0; j < n.getDimension(); j++) {
+                System.out.print(y[i][j] + " ");
             }
 
             System.out.println();
         }
-
-
     }
 }
